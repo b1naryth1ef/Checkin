a2615fea31a335bea735921c1b84ddbcd1b6462d
Andrei Z <b1naryth1ef@gmail.com>
2011-07-08 22:35:36 -0500

    Added checkinmode() which handles our checkins. Got the new user function working.
    Signed-off-by: Andrei Z <b1naryth1ef@gmail.com>

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 from time import *
 import random
-import save
+import db
 
 t = gmtime()
 date = str(t[0])+"/"+str(t[1])+"/"+str(t[2])
 
-def imp():
-	pass
-
-def exp():
-	pass
-def create_user(fname,lname,id):
-	pass
+def checkinmode():
+	iny = raw_input("NAME:   ")
+	def markin(uid,current):
+		current += 1
+		db.checkin(current,uid)
+		checkinmode()
+	try:
+		x = db.find_user("name",iny)
+		markin(x.id,x.checkins)
+	except: #@Error 001
+		print "EXCEPTION ERROR 001 [Report to admin please!]"
+		checkinmode()
 
 def home():
 	print "[N]ew User" 
 	print "[L]ist Users"
 	print "[S]earch"
 	print "[C]heck-in mode"
+	print "[E]dit user"
 	xin = raw_input("Input:   ")
 	if xin in ("N","n"):
 		home_newuser()
@@ -27,9 +33,19 @@ def home():
 		home_search()
 	if xin in ("C","c"):
 		home_checkin()
+	if xin in ("E","e"):
+		home_edituser()
 
 def home_newuser():
-	new = type_user("Andrei", "Zbikowski")
+	print "--->NEW USER MENU<---"
+	print "====================="
+	newfname = raw_input("NEW USER FIRST NAME:   ")
+	newlname = raw_input("NEW USER LAST NAME:   ")
+	today = raw_input("MARK PRESENT FOR TODAY [Y/N]   ")
+	if today in ("Y","y"):
+		db.add_user(newfname,newlname,"1")
+	else:
+		db.add_user(newfname,newlname,"0")
 
 def home_list():
 	pass
@@ -38,6 +54,9 @@ def home_search():
 	pass
 
 def home_checkin():
+	checkinmode()
+
+def home_edituser():
 	pass
 
 home()
