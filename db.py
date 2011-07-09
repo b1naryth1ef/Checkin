5e9b70d75af871737abe6ff921ff51bfd0e47ca0
Andrei Z <b1naryth1ef@gmail.com>
2011-07-08 22:36:40 -0500

    Added name field. added checkin() which handles checking users in, in a simplistic way. Added dfind_user() which allows for multiple fields.
    Signed-off-by: Andrei Z <b1naryth1ef@gmail.com>

--- db.py
+++ db.py
@@ -6,29 +6,41 @@ c = db.cursor()
 
 class User(object):
     """A simple User class"""
-    def __init__(self, userid,first_name=None, last_name=None,checkins=None):
+    def __init__(self, userid, name=None, first_name=None, last_name=None,checkins=None):
         self.id = userid
+        self.name = name
         self.first_name = first_name
         self.last_name = last_name
         self.checkins = checkins
 
 def get_users():
 	c.execute("""SELECT * FROM users""")
-	for userid, first_name, last_name, company in c.fetchall():
-		u = User(userid, first_name, last_name)
+	for id, name, first_name, last_name, checkins in c.fetchall():
+		u = User(id, name, first_name, last_name, checkins)
     	print u.first_name
 
-def add_user(zid,fname,lname,checkins):
-	c.executemany("""INSERT INTO users (id, first_name, last_name, checkins) VALUES (%s, %s, %s, %s)""", [(zid, fname, lname, checkins)])
+def add_user(fname,lname,checkins):
+	name = fname+" "+lname
+	c.executemany("""INSERT INTO users ( name, first_name, last_name, checkins) VALUES (%s, %s, %s, %s)""", [(name, fname, lname, checkins)])
 
-def update_user(uid,field,change):
-	"""Update a row in format ID|FIELD|CHANGE"""
-	c.execute("UPDATE Users SET %s = %s WHERE Id = %s", (field, change, uid)) 
+def update_user(change_field,change,iden_field,iden):
+	"""Update a row in format IDENITY|SELECT FIELD|CHANGE|CHANGE FIELD"""
+	c.execute("""UPDATE Users SET %s = "%s" WHERE %s = "%s" """, (change_field,change,iden_field,iden)) 
+	db.commit()
+
+def checkin(change,iden):
+	"""Checkin a user"""
+	c.execute("""UPDATE Users SET checkins = "%s" WHERE id = "%s" """, (change,iden)) 
 	db.commit()
 
 def find_user(field,value):
 	c.execute("""SELECT * FROM Users WHERE %s = "%s" """ % (field,value))
-	for userid, first_name, last_name, checkins in c.fetchall():
-		u = User(id, first_name, last_name, checkins)
+	for id, name, first_name, last_name, checkins in c.fetchall():
+		u = User(id, name, first_name, last_name, checkins)
 		return u
 
+def dfind_user(field1,field2,value1,value2):
+	c.execute("""SELECT * FROM Users WHERE %s = %s AND %s = %s""", (field1, value1, field2, value2))
+	for userid, first_name, last_name, checkins in c.fetchall():
+		u = User(userid, first_name, last_name, checkins)
+		return u
