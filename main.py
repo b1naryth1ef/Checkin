4fec7608cc22288f885f25a8b87daf13e9a7b637
Andrei Z <b1naryth1ef@gmail.com>
2011-07-09 20:27:42 -0500

    Implemented the better home menu from the repo of @harryd
    Signed-off-by: Andrei Z <b1naryth1ef@gmail.com>

--- main.py
+++ main.py
@@ -56,33 +56,6 @@ def checkinmode():
 		ui.err("#002")
 		time.sleep(1)
 		sys.exit()
-	
-
-def home():
-	clear()
-	print "[N]ew User" 
-	print "[L]ist Users"
-	print "[S]earch"
-	print "[C]heck-in mode"
-	print "[E]dit user"
-	print "[Q]uit"
-	xin = raw_input("Input:   ")
-	if xin in ("N","n"):
-		home_newuser()
-	elif xin in ("L","l"):
-		home_list()
-	elif xin in ("S","s"):
-		home_search()
-	elif xin in ("C","c"):
-		home_checkin()
-	elif xin in ("E","e"):
-		home_edituser()
-	elif xin in ("Q","q"):
-		sys.exit()
-	else:
-		raw_input("Whoops... Didnt get that. Press [enter] to try again.")
-		home()
-
 
 def home_newuser():
 	print "====================="
@@ -147,5 +120,26 @@ def home_checkin():
 
 def home_edituser():
 	pass
+
+def home():
+	clear()
+	print "[N]ew User" 
+	print "[L]ist Users"
+	print "[S]earch"
+	print "[C]heck-in mode"
+	print "[E]dit user"
+	print "[Q]uit"
+	d = {'n': home_newuser,
+		'l': home_list,
+		's': home_search,
+		'c': home_checkin,
+		'e': home_edituser,
+		'q': sys.exit}
+	xin = raw_input("Input:   ").lower()
+	if xin in d:
+		d[xin]()
+	else:
+		raw_input("Whoops... Didnt get that. Press [enter] to try again.")
+		home()
 
 home()
